2a2dafa661cfb48e125e135837c2cd229bdb6aaa
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-10-10 14:05:06 +0800

    chore: trigger actions

--- main.js
+++ main.js
@@ -3,4 +3,5 @@ import "./style.css";
 document.querySelector("#app").innerHTML = `
   <h1>${import.meta.env.VITE_MY_NAME}</h1>
   <h1>${import.meta.env.VITE_INFO}</h1>
+  <h1>trigger action</h1>
 `;
